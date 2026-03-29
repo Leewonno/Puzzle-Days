c297cebe14badb5f3c7b9f44b24c926e3df3b010
Leewonno <dldnjssh123@naver.com>
2026-03-29 23:56:45 +0900

    [UPDATE] 구글 ADMOB 설정

--- src/utils/admob.ts
+++ src/utils/admob.ts
@@ -2,13 +2,13 @@ import { Capacitor } from "@capacitor/core";
 
 const INTERSTITIAL_AD_UNIT_ID =
   Capacitor.getPlatform() === "ios"
-    ? "ca-app-pub-3940256099942544/4411468910" // iOS 테스트 ID
-    : "ca-app-pub-3940256099942544/1033173712"; // Android 테스트 ID
+    ? "ca-app-pub-3510048415455621/8579353898" // iOS 테스트 ID
+    : "ca-app-pub-3510048415455621/8718954690"; // Android 테스트 ID
 
 const BANNER_AD_UNIT_ID =
   Capacitor.getPlatform() === "ios"
-    ? "ca-app-pub-3940256099942544/2934735716" // iOS 배너 테스트 ID
-    : "ca-app-pub-3940256099942544/6300978111"; // Android 배너 테스트 ID
+    ? "ca-app-pub-3510048415455621/4779709687" // iOS 배너 테스트 ID
+    : "ca-app-pub-3510048415455621/3143194385"; // Android 배너 테스트 ID
 
 export async function showInterstitialAd(): Promise<void> {
   if (!Capacitor.isNativePlatform()) return;
@@ -17,7 +17,7 @@ export async function showInterstitialAd(): Promise<void> {
 
   const options = {
     adId: INTERSTITIAL_AD_UNIT_ID,
-    isTesting: true, // 출시 전 반드시 false로 변경
+    isTesting: false,
   };
 
   await AdMob.prepareInterstitial(options);
@@ -27,13 +27,14 @@ export async function showInterstitialAd(): Promise<void> {
 export async function showBannerAd(): Promise<void> {
   if (!Capacitor.isNativePlatform()) return;
 
-  const { AdMob, BannerAdSize, BannerAdPosition } =
-    await import("@capacitor-community/admob");
+  const { AdMob, BannerAdSize, BannerAdPosition } = await import(
+    "@capacitor-community/admob"
+  );
   await AdMob.showBanner({
     adId: BANNER_AD_UNIT_ID,
     adSize: BannerAdSize.ADAPTIVE_BANNER,
     position: BannerAdPosition.BOTTOM_CENTER,
-    isTesting: true,
+    isTesting: false,
   });
 }
 
